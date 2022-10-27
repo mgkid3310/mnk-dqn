be074ea046f15b9335de95ae9d47de03dabed7b0
Orbis2358 <mgkid3310@naver.com>
2022-10-28 02:53:59 +0900

    add check result function

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ class mnk_env(gym.Env):
     reward = (-1, 0, 1)
 
     def __init__(self, m = 3, n = 3, k = 3):
+        self.shape = m, n
         self.size = m * n
         self.k = k
         self.turn = 1
@@ -14,20 +15,53 @@ class mnk_env(gym.Env):
 
     def reset(self):
         self.board = np.zeros(self.size, dtype = np.int8)
+        self.array = self.board.reshape(self.shape)
         self.observation = np.zeros(self.size, dtype = np.int8)
 
     def get_obs(self):
         return self.board, self.turn
 
-    def check_result(self):
-        done = False
-        winner = 0
+    def check_result(self, action):
+        turn = self.board[action]
+        x, y = action // self.shape[0], action % self.shape[0]
+        self.array = self.board.reshape(self.shape)
+
+        done, winner = False, 0
+        # print((x, y))
+        # print(self.array)
+
+        lists = [np.array([], dtype = np.int8)] * 4
+        for i in range(1 - self.k, self.k):
+            lists[0] = self.append_if_valid(lists[0], x + i, y)
+            lists[1] = self.append_if_valid(lists[1], x, y + i)
+            lists[2] = self.append_if_valid(lists[2], x + i, y + i)
+            lists[3] = self.append_if_valid(lists[3], x + i, y - i)
+
+        for list in lists:
+            # print(list)
+            i, n = 0, 0
+            for i in range(len(list)):
+                if list[i] == turn:
+                    n += 1
+                    if n >= self.k:
+                        done, winner = True, turn
+                        break
+                else:
+                    n = 0
+
+        # print((done, winner))
 
         return done, winner
 
+    def append_if_valid(self, list, x, y):
+        if 0 <= x < self.shape[0] and 0 <= y < self.shape[1]:
+            list = np.append(list, self.array[x, y])
+
+        return list
+
     def step(self, action):
         self.board[action] = self.turn
-        self.done, winner = self.check_result()
+        self.done, winner = self.check_result(action)
         reward = self.turn * winner
 
         self.turn = -self.turn
@@ -38,6 +72,10 @@ class mnk_env(gym.Env):
         return np.where(self.board == 0)
 
 if __name__ == '__main__':
-    env = mnk_env(3, 3, 3)
-    print(env.step(1))
-    print(env.step(3))
+    env = mnk_env(4, 4, 3)
+    env.step(1)
+    env.step(3)
+    env.step(4)
+    env.step(6)
+    env.step(7)
+    env.step(9)
